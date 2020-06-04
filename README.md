14c8d4b5e461e029d6998b37a770d87865a61f9c
marylandgirl <kimbussie@yahoo.com>
2020-06-04 10:48:37 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+### This project is a template for incorporating Spring Boot Security into a application.
+
+To make use of it, clone it and build your application on top of it.
---
+++ src/main/java/com/example/demo/HomeController.java
@@ -0,0 +1,76 @@
+package com.example.demo;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.validation.Valid;
+import java.security.Principal;
+import java.util.HashSet;
+import java.util.Set;
+
+@Controller
+public class HomeController {
+
+    @Autowired
+    UserRepository userRepository;
+
+    @Autowired
+    RoleRepository roleRepository;
+
+    @RequestMapping("/")
+    public String index() {
+        return "index";
+    }
+
+    @RequestMapping("/login")
+    public String login() {
+        return "login";
+    }
+
+    @PostMapping("logout")
+    public String logout() {
+        return "redirect:/login?logout=true";
+    }
+
+    @RequestMapping("/admin")
+    public String admin(){
+        return "admin";
+    }
+
+    @RequestMapping("/secure")
+    public String secure(Principal principal, Model model) {
+        String username = principal.getName();
+        model.addAttribute("user",userRepository.findByUsername(username));
+            return "secure";
+    }
+
+    @GetMapping("/register")
+    public String showRegistrationPage(Model model) {
+        model.addAttribute("user", new User());
+        return "register";
+    }
+
+    @PostMapping("/register")
+    public String processRegistrationPage(@Valid @ModelAttribute("user") User user,
+                                          BindingResult result, Model model) {
+        if(result.hasErrors()) {
+            return "register";
+        } else {
+            model.addAttribute("message", "User Account Created");
+            user.setEnabled(true);
+            Role role = new Role(user.getUsername(), "ROLE_USER");
+            Set<Role> roles = new HashSet<Role>();
+            roles.add(role);
+            roleRepository.save(role);
+            userRepository.save(user);
+        }
+        return "index";
+    }
+
+}
